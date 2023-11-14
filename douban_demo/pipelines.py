3219db908616fc0227f43fae8ae63745c0b7a37c
zqbinary <zqbinary@foxmail.com>
2023-11-14 15:15:56 +0800

    db pipeline

--- douban_demo/pipelines.py
+++ douban_demo/pipelines.py
@@ -6,11 +6,47 @@
 # from itemadapter import ItemAdapter
 
 import openpyxl
+import pymysql
 
 
-class DoubanDemoPipeline:
+class DbPipeline:
     def __init__(self):
+        self.conn = pymysql.connect(host='localhost',
+                                    user='root',
+                                    password='root',
+                                    database='heima')
+        self.cursor = self.conn.cursor(cursor=pymysql.cursors.DictCursor)
+        self.data = []
 
+    # 我建议这里用sqlalchemy框架
+    def process_item(self, item, spider):
+        title = item.get('title', '')
+        rank = item.get('rank', '')
+        subject = item.get('subject', '')
+        self.data.append((title, rank, subject))
+        if 100 == len(self.data):
+            self._write_to_db()
+        return item
+
+    def _write_to_db(self):
+        # 准备一个容器比如100条，然后插入一次
+        if not len(self.data):
+            return
+        self.cursor.executemany(
+            'insert into movie (title,rating,subject) values (%s,%s,%s)',
+            self.data
+        )
+        # 这里后后面
+        self.conn.commit()
+        self.data.clear()
+
+    def close_spider(self, spider):
+        self._write_to_db()
+        self.conn.close()
+
+
+class ExcelPipeline:
+    def __init__(self):
         self.wb = openpyxl.Workbook()
         self.ws = self.wb.active
         self.ws.append(('标题', '评分', '主题'))
--- douban_demo/settings.py
+++ douban_demo/settings.py
@@ -66,7 +66,8 @@ DEFAULT_REQUEST_HEADERS = {
 # Configure item pipelines
 # See https://docs.scrapy.org/en/latest/topics/item-pipeline.html
 ITEM_PIPELINES = {
-    "douban_demo.pipelines.DoubanDemoPipeline": 300,
+    # "douban_demo.pipelines.ExcelPipeline": 300,
+    "douban_demo.pipelines.DbPipeline": 300,
 }
 
 # Enable and configure the AutoThrottle extension (disabled by default)
